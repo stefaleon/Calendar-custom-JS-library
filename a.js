53af0f32523c35166ab6657ba1427f7d7581b579
unknown <stefaleon@gmail.com>
2021-05-17 20:44:52 +0300

    daysInMonth

--- a.js
+++ a.js
@@ -2,13 +2,13 @@ const lib = require('./lib');
 
 // destructuring
 const { days } = lib.data;
-const { firstDay } = lib.methods;
+const { daysInMonth } = lib.methods;
 
 console.log('getDay() day mapping: Sun to Sat -> 0 to 6');
 console.log('getMonth() month mapping: Jan to Dec -> 0 to 11');
-let res = firstDay(2021, 4); // month 4 is the fifth month of the year - May
-console.log('May 2021 - First day is', res, 'which is a', days[res]);
-res = firstDay(2021, 1); // month 1 February
-console.log('Feb 2021 - First day is', res, 'which is a', days[res]);
-res = firstDay(2021, 8); // month 8 september
-console.log('Sep 2021 - First day is', res, 'which is a', days[res]);
+let res = daysInMonth(2021, 4); // month 4 is the fifth month of the year - May
+console.log('May 2021 has', res, 'days');
+res = daysInMonth(2021, 1); // month 1 February
+console.log('Feb 2021 has', res, 'days');
+res = daysInMonth(2020, 1); // month 1 February, 2020 is a leap year
+console.log('Feb 2020 has', res, 'days');
--- lib.js
+++ lib.js
@@ -22,14 +22,34 @@ const months = [
   'December',
 ];
 
+/*
+Arguments: 4 digit year
+Return: an array of 12 numbers, each represents the
+number of days for each month of the specific year
+*/
 function countOfDaysPerMonth(year) {
   return [31, februaryDaysCount(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 }
 
+/*
+Returns the first day of the month 
+Arguments: 4 digit year, Jan to Dec -> 0 to 11
+Return: Sun to Sat -> 0 to 6
+*/
 function firstDay(year, month) {
   const fd = new Date(year, month);
   return fd.getDay();
 }
 
+/*
+https://stackoverflow.com/questions/1184334/get-number-days-in-a-specified-month-using-javascript
+Returns the number of days of the month 
+Arguments: 4 digit year, Jan to Dec -> 0 to 11
+Return: number indicating the count of days in the specific month
+*/
+function daysInMonth(year, month) {
+  return new Date(year, month + 1, 0).getDate();
+}
+
 exports.data = { days, months };
-exports.methods = { countOfDaysPerMonth, firstDay };
+exports.methods = { countOfDaysPerMonth, firstDay, daysInMonth };

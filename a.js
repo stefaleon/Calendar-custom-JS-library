37e769837b82e13dd5cf8e94e57869fa6ac1ff0f
stefaleon <stefaleon@gmail.com>
2021-05-18 11:11:48 +0300

    rename to fill6x7 and move to lib

--- a.js
+++ a.js
@@ -1,45 +1,6 @@
 const lib = require('./lib');
 
 // destructuring
-const { days } = lib.data;
-const { firstDay, daysInMonth } = lib.methods;
+const { fill6x7 } = lib.methods;
 
-/*
-Fill the monthly calendar array 
-with 6 week arrays 
-each containing the dates for each week
-in the appropriate position for 
-mapping: Sun to Sat -> 0 to 6
-*/
-function fillMonthlyCalendarPage(year, month) {
-  const day1 = firstDay(year, month);
-  const monthlyCalendar = [];
-  let date = 1;
-
-  for (let i = 0; i < 6; i++) {
-    // create a week array
-    let week = [];
-
-    /*
-    add dates to week
-    fill with null values the date placeholders
-    before the first day of the month
-    and after the last day of the month
-    */
-    for (let j = 0; j < 7; j++) {
-      if ((i === 0 && j < day1) || date > daysInMonth(year, month)) {
-        week.push(null);
-      } else {
-        week.push(date);
-        date++;
-      }
-    }
-
-    // add the week to the monthly calendar array
-    monthlyCalendar.push(week);
-  }
-
-  return monthlyCalendar;
-}
-
-console.log(fillMonthlyCalendarPage(2021, 3)); // April 2021
+console.log(fill6x7(2021, 3)); // April 2021
--- lib.js
+++ lib.js
@@ -51,5 +51,43 @@ function daysInMonth(year, month) {
   return new Date(year, month + 1, 0).getDate();
 }
 
+/*
+Fill the monthly calendar array 
+with 6 week arrays 
+each containing the dates for each week
+in the appropriate position for 
+mapping: Sun to Sat -> 0 to 6
+*/
+function fill6x7(year, month) {
+  const day1 = firstDay(year, month);
+  const monthlyCalendar = [];
+  let date = 1;
+
+  for (let i = 0; i < 6; i++) {
+    // create a week array
+    let week = [];
+
+    /*
+    add dates to week
+    fill with null values the date placeholders
+    before the first day of the month
+    and after the last day of the month
+    */
+    for (let j = 0; j < 7; j++) {
+      if ((i === 0 && j < day1) || date > daysInMonth(year, month)) {
+        week.push(null);
+      } else {
+        week.push(date);
+        date++;
+      }
+    }
+
+    // add the week to the monthly calendar array
+    monthlyCalendar.push(week);
+  }
+
+  return monthlyCalendar;
+}
+
 exports.data = { days, months };
-exports.methods = { countOfDaysPerMonth, firstDay, daysInMonth };
+exports.methods = { countOfDaysPerMonth, firstDay, daysInMonth, fill6x7 };
